48e6214d35a3d6a23a42bc15c4cf703b1d7dcf6b
김지섭 <cocacola158500@gmail.com>
2023-05-07 09:57:40 +0900

    채팅방, 채팅내용

---
+++ src/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
+  });
+  socket.on("room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
+  });
+});
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//   socket["nickname"] = "Anon";
+//   sockets.push(socket);
+//   console.log("Connected to Browser");
+//   socket.on("close", closeServer);
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aScoket) =>
+//           aScoket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
+httpServer.listen(3000, handleListen);
